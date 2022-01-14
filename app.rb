5a66a494a9eb2c01ed93987109d0f7df9e859a26
Josh <joshlim092@gmail.com>
2022-01-14 11:39:19 +0000

    add function to edit bookmark title and url

--- app.rb
+++ app.rb
@@ -33,6 +33,21 @@ class BookmarkManager < Sinatra::Base
   post '/bookmarks' do
     Bookmark.create(url: params[:URL], title: params[:title])
     redirect '/bookmarks'
+  end
+
+  get '/bookmarks/:id/edit' do 
+    @bookmark_id = params[:id]
+    erb :'bookmarks/edit'
+  end 
+
+  patch '/bookmarks/:id' do 
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    #insert SQL query with exec_params security 
+    connection.exec_params(
+      "UPDATE bookmarks SET url = $1, title = $2 WHERE id = $3",
+      [ params[:url], params[:title], params[:id] ]
+    )
+    redirect ('/bookmarks')
   end 
 
   run! if app_file == $0
--- spec/features/updating_a_bookmark_spec.rb
+++ spec/features/updating_a_bookmark_spec.rb
@@ -1,13 +1,19 @@
 feature 'Updating a bookmark' do 
   scenario 'it changes a bookmark' do 
     bookmark = Bookmark.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
-    visit '/bookmarks'
-    expect(page). to have_link(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    visit('/bookmarks')
+    expect(page).to have_link('Makers Academy', href: 'http://www.makersacademy.com')
 
     first('.bookmark').click_button 'Edit'
-    expect()
+    expect(current_path).to eq "/bookmarks/#{bookmark.id}/edit"
 
+    fill_in('url', with: "http://www.snakersacademy.com")
+    fill_in('title', with: "Snakers Academy")
+    click_button('Submit')
 
+    expect(current_path).to eq '/bookmarks'
+    expect(page).not_to have_link('Makers Academy', href: 'http://www.makersacademy.com')
+    expect(page).to have_link('Snakers Academy', href: 'http://www.snakersacademy.com')
 
   end 
 end 
